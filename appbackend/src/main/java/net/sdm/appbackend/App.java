ccc2094d737210d7eb4239737a42efbf57d2b5fc
mohammed <sadam.mohammed909@gmail.com>
2019-09-22 19:16:46 +0530

    My project setup completed

---
+++ appbackend/src/main/java/net/sdm/appbackend/App.java
@@ -0,0 +1,13 @@
+package net.sdm.appbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
